ef8946b2778159b260b80b599d156c2816e3fc22
lincgr <lincgr@gmail.com>
2024-10-08 13:29:58 +0200

    03_01 Crear un DataFrame en PySpark

---
+++ 03_01/03_01.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("AppSpark") \
+    .getOrCreate()
+
+datos = [("Juan", 28, "Ingeniero"),
+         ("Ana", 23, "Diseñadora"),
+         ("Luis", 35, "Arquitecto"),
+         ("Carlos",45,"Analista"),
+         ("Laura",30,"Desarrolladora"),
+         ("Mateo",25,"Diseñador"),
+        ("Sofía",38,"Gerente de Proyecto"),
+        ("Pedro",50,"Director"),
+        ("Elena",27,"Marketing"),
+        ("José",33,"Consultor"),
+        ("Lucía",29,"Administradora")]
+
+nombre_columnas = ["Nombre", "Edad", "Profesión"]
