f0a061fd95a9fbc044f8118f0f9bc02ee873f75f
lincgr <lincgr@gmail.com>
2024-10-08 15:56:49 +0200

    03_08 Operaciones Join en un DataFrame en PySpark

---
+++ 03_08/03_08.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("AppSpark") \
+    .getOrCreate()
+
+clientes = [("1", "Juan", "España"),
+            ("2", "Ana", "México"),
+            ("3", "Luis", "Colombia")]
+
+compras = [("1", "Teléfono", 1000),
+           ("2", "Computadora", 1500),
+           ("1", "Monitor", 550), 
+           ("4", "Audífonos", 360)]
+
+df_clientes = spark.createDataFrame(clientes, ["Cliente_ID", "Nombre", "País"])
+df_compras = spark.createDataFrame(compras, ["Cliente_ID", "Producto", "Monto"])
